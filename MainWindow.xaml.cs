ac171468b4c2561cff3ddc332b7e9655d1a53411
Lamborge <maxpopelov@gmail.com>
2020-09-10 16:00:49 +0300

    added basic functional

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,49 @@ namespace FindSimilarity
             InitializeComponent();
         }
 
+        private void FinisButton_Click(object sender, RoutedEventArgs e)
+        {
+            string FirstString = FirstTextBox.Text; // строка для первого текста
+            string SecondString = SecondTextBox.Text; // строка для второго текста
 
+            char[] first_arr = FirstString.ToCharArray();
+            char[] second_arr = SecondString.ToCharArray();
+
+            int res = 0;
+
+            if (first_arr.Length < second_arr.Length)
+            {
+
+                float symbol_proc = 100 / first_arr.Length;
+
+                for (int i = 0; i < first_arr.Length; i++)
+                {
+                    if (first_arr[i] == second_arr[i])
+                    {
+                        res++;
+                    }
+                }
+
+                MessageBox.Show("Тексты совпадают на " + Convert.ToString(res * symbol_proc) + "%");
+            }
+            else
+            {
+                
+                float symbol_proc = 100 / second_arr.Length;
+
+                for (int i = 0; i < second_arr.Length; i++)
+                {
+                    if (first_arr[i] == second_arr[i])
+                    {
+                        res++;
+                    }
+                }
+
+                MessageBox.Show("Тексты совпадают на " + Convert.ToString(res * symbol_proc) + "%");
+            }
+
+            //MessageBox.Show(res.ToString());
+
+        }
     }
 }
